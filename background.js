025f83ecf01ec33ae5077d10505fb7b73110cb1a
rycont <reactdev@kakao.com>
2020-06-10 08:00:20 +0900

    init

---
+++ background.js
@@ -0,0 +1,12 @@
+let err = 0
+const repeater = async () => {
+  try {
+    await (await fetch('/StudentHome')).text()
+    console.log('정상적으로 시간이 초기화되었습니다')
+  } catch (e) {
+    alert('시간을 초기화하는데 문제가 발생했습니다.')
+    err++
+  }
+}
+
+chrome.storage.sync.get(() => setInterval(() => !err && repeater(), 10 * 60 * 1000))
